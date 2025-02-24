2e4018104f6f5ff8d756103c6174381621a4d658
DIVYANGSHR <divyangshrivastav1998@gmail.com>
2025-02-24 18:45:55 +0530

    1433RMRP0F

---
+++ FORM_VALIDATION/core/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.core import validators
+
+
+def start_with_a(val):
+    x= val[0]
+    if x.isdigit():
+        raise forms.ValidationError( 'Name don"t start with number ')
+    
+
+class MarvelForm(forms.Form):
+    name = forms.CharField(validators=[start_with_a])
+    heroic_name = forms.CharField(validators=[validators.MaxLengthValidator(5)])
