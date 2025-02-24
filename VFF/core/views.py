db08c221d8070fc8b86d1f0638499543b0f8f906
DIVYANGSHR <divyangshrivastav1998@gmail.com>
2025-02-24 17:14:03 +0530

    prrddg

--- VFF/core/views.py
+++ VFF/core/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from .forms import MarvelForm
+from . forms import MarvelForm
 
-# Create your views here.
 def index (request):
-    if  request.method == 'POST':
+    if request.method == 'POST':
         mf = MarvelForm(request.POST)
         if mf.is_valid():
             print(mf.cleaned_data['name'])
-            print(mf.cleaned_data['password'])
+            print(mf.changed_data['password'])
             print(mf.cleaned_data['confirm_password'])
             mf =MarvelForm()
+
     else:
         mf= MarvelForm()
+    return render (request,'core/index.html',{'mf':mf})
 
-    return render(request,'core/index.html' {'mf',: mf})
+
+
+
